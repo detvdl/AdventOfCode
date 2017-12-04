da9d26ecac0cb3f7c673f232a8c5b6e26870c842
detvdl <detlev.vandaele@gmail.com>
2017-12-04 09:52:53 +0100

    Hlep

--- day4/day4.sh
+++ day4/day4.sh
@@ -1,26 +1,22 @@
 #!/usr/bin/env bash
 
-function duplicates {
-    dups=$(echo "$1" | tr "[:blank:]" " " | tr "[:blank:]" "\n" | sort | uniq -d)
-    [ -z "$dups" ] && return 1 || return 0
-}
-
-function anagrams {
-    sorted=""
-    for word in $1; do
-        sorted+=$(echo "$word" | grep -o . | sort | tr -d "\n")
-        sorted+=" "
-    done
-    return $(duplicates "$sorted")
-}
-
-valid_dups=0
-valid_ana=0
+valid_dups=0; valid_ana=0
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    # Puzzle 1
-    ! duplicates "$line" && ((valid_dups++))
-    # Puzzle 2
-    ! anagrams "$line" && ((valid_ana++))
+    br1=0; br2=0
+    sorted=""
+    for word in $line; do
+        # Puzzle 1
+        !(( br1 )) && br1=$(grep -ow "$word" <<< "$line" | wc -l) && (( br1-- ))
+
+        # Puzzle 2
+        sort_word=$(echo "$word" | grep -o . | sort | tr -d "\n")
+        !(( br2 )) && br2=$(grep -ow "$sort_word" <<< "$sorted" | wc -l)
+
+        (( br1 )) && (( br2 )) && break
+        sorted+="$sort_word "
+    done
+    !(( br1 )) && (( valid_dups++ ))
+    !(( br2 )) && (( valid_ana++ ))
 done < "$1"
 
 echo "Puzzle 1: $valid_dups passphrases are valid!"
