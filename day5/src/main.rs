34eaf72e9428bc4a00cffe3141a60e5b49203f76
detvdl <detlev.vandaele@gmail.com>
2017-12-05 22:03:55 +0100

    Clean this bad boy up a little

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,38 +4,23 @@ use std::io::prelude::*;
 
 fn main() {
     let file = File::open("input").unwrap();
-    let mut bufread = BufReader::new(file);
-    let mut contents = String::new();
-    bufread.read_to_string(&mut contents).unwrap();
-
-    let jumps1 = parse_input(contents);
-    let start = 0;
-    let jumps2 = jumps1.clone();
+    let reader = BufReader::new(file);
+    let mut jumps: Vec<i32> = reader.lines()
+        .map(|num| num.unwrap().parse::<i32>().unwrap())
+        .collect();
+    let mut jumps2 = jumps.clone();
     
-    let steps1 = count_steps_1(start, jumps1);
+    let steps1 = count_steps_1(&mut jumps);
     println!("Puzzle 1: The amount of steps required to exit the maze is: {}", steps1);
 
-    let steps2 = count_steps_2(start, jumps2);
+    let steps2 = count_steps_2(&mut jumps2);
     println!("Puzzle 2: The amount of steps required to exit the maze is: {}", steps2);
 
 }
 
-fn parse_input(contents: String) -> Vec<i32> {
-    let mut parsed = Vec::new();
-    let split = contents.split("\n");
-    for step in split {
-        let val = step.parse();
-        match val {
-            Ok(v) => parsed.push(v),
-            Err(_) => {}
-        }
-    }
-    parsed
-}
-
-fn count_steps_1(start: usize, mut jumps: Vec<i32>) -> u32 {
+fn count_steps_1(jumps: &mut Vec<i32>) -> u32 {
     let mut steps: u32 = 0;
-    let mut cur_ind = start as i32;
+    let mut cur_ind: i32 = 0;
 
     while let Some(cur_val) = jumps.get_mut(cur_ind as usize) {
         cur_ind += *cur_val;
@@ -46,9 +31,9 @@ fn count_steps_1(start: usize, mut jumps: Vec<i32>) -> u32 {
     steps
 }
 
-fn count_steps_2(start: usize, mut jumps: Vec<i32>) -> u32 {
+fn count_steps_2(jumps: &mut Vec<i32>) -> u32 {
     let mut steps: u32 = 0;
-    let mut cur_ind = start as i32;
+    let mut cur_ind: i32 = 0;
 
     while let Some(cur_val) = jumps.get_mut(cur_ind as usize) {
         cur_ind += *cur_val;
