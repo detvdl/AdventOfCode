6567f52a935501c45269a36ff4e966946332b7da
detvdl <detlev.vandaele@gmail.com>
2017-12-08 12:41:23 +0100

    try my hand at a more immutable day6 approach

--- day6/src/day6/core.clj
+++ day6/src/day6/core.clj
@@ -3,14 +3,12 @@
 
 (require '[clojure.string :as str])
 
-(defn read-file-to-array [path]
-  (let [file (slurp path)
-        nums (str/split file #"\s")]
-    (map #(Integer/parseInt %) nums)))
+(defn parse-file [path]
+  (mapv #(Integer/parseInt %)
+        (str/split (slurp path) #"\s+")))
 
-(defn seen? [lst hashset]
-  (some? (get hashset lst)))
-
+;; Version 1: using transient vectors
+;; for internal mutability
 (defn redistribute [lst]
   (let [maxv (apply max lst)
         pos (.indexOf lst maxv)
@@ -23,12 +21,29 @@
 
 (defn count-steps [lst]
   (loop [lst lst hs (hash-set []) cnt 0]
-    (if (seen? lst hs)
+    (if (contains? hs lst)
       [cnt lst]
       (recur (redistribute lst) (conj hs (into [] lst)) (+ cnt 1)))))
 
+;; Version 2: create new lists (immutable)
+(defn redistribute-imut [lst]
+  (let [maxm (apply max lst)
+        ind (.indexOf lst maxm)]
+    (->> (concat (repeat ind 0) [(- maxm)] (repeat maxm 1))
+         (partition (count lst) (count lst) (repeat 0))
+         (apply mapv + lst))))
+
+(defn count-steps-imut [lst]
+  (loop [lst lst, seen {lst 0}, cnt 1]
+    (let [lst* (redistribute-imut lst)]
+      (if (seen lst*)
+        {:puzzle1 cnt :puzzle2 (- (+ cnt 1) (seen lst*))}
+        (recur lst* (assoc seen lst cnt) (inc cnt))))))
+
 (defn -main [& args]
-  (let [arr (read-file-to-array "input")
+  (let [arr (parse-file "input")
         [steps lst] (count-steps (vec arr))
-        [sec _] (count-steps lst)]
-    (printf "Puzzle 1: %d\nPuzzle 2: %d\n" steps sec)))
+        [sec _] (count-steps lst)
+        v2 (count-steps-imut arr)]
+    (printf "Puzzle 1v1: %d\nPuzzle 2v1: %d\n" steps sec)
+    (printf "Puzzle 1v2: %d\nPuzzle 2v2: %d" (v2 :puzzle1) (v2 :puzzle2))))
