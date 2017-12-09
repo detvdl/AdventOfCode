946f40374cd19b825455f4c8e4398625a2b3d020
detvdl <detlev.vandaele@gmail.com>
2017-12-09 11:13:25 +0100

    minor style modifications

--- day9/day9.rb
+++ day9/day9.rb
@@ -2,16 +2,16 @@ line = File.read("input")
 
 cancelled = line.gsub(/\!.{1}/, '')
 garb_count = 0
-garbage = cancelled.gsub(/\<([^\>]*)\>/) { || garb_count += $+.length; '' }
+garbage = cancelled.gsub(/\<([^\>]*)\>/) { garb_count += $+.length; '' }
 groups = Array.new
 val = 0
-garbage.chars.each { |char|
+garbage.chars.each do |char|
   if char == '{'
     groups.push("{")
   elsif char == '}'
     val += groups.length
     groups.pop()
   end
-}
+end
 puts "The total value of the groups is #{val}."
 puts "The total garbage count is #{garb_count}."
