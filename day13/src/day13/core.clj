04ff8f371b1534f3dc4c2cc093f8c53c8bce276e
detvdl <detlev.vandaele@gmail.com>
2017-12-13 18:10:35 +0100

    try out transducers & destructuring in argument list

--- day13/src/day13/core.clj
+++ day13/src/day13/core.clj
@@ -7,8 +7,8 @@
 (defn parse-input [path]
   (partition 2 (map #(Integer/parseInt %) (str/split (slurp path) #"\n|: "))))
 
-(defn caught? [step delay]
-  (= (mod (+ delay (first step)) (* (- (second step) 1) 2)) 0))
+(defn caught? [[step range] delay]
+  (zero? (mod (+ delay step) (* (- range 1) 2))))
 
 (defn avoid-all? [steps delay]
   (not-any? #(caught? % delay) steps))
@@ -20,11 +20,21 @@
           0
           layers))
 
+(def xf (comp (filter #(caught? % 0)) (map #(apply * %))))
+(defn solve1-trans [layers]
+  (transduce xf + layers))
+
 (defn solve1-filter [layers]
   (apply + (map #(apply * %) (filter #(caught? % 0) layers))))
 
 (defn solve2 [layers]
   (first (filter some? (map #(if (avoid-all? layers %) %) (range)))))
+
+(defn solve2-anonfn [layers]
+  (some (fn [delay]
+          (when (not-any? (fn [step] (caught? step delay)) layers)
+            delay))
+        (range)))
 
 (defn -main [& args]
   (let [layers (parse-input "input")]
